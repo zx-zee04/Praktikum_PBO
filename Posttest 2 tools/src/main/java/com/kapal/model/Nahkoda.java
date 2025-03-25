36ffe382c202d20d6d60cf6a07a5582b7a36a552
zx-zee04 <mnrdhan04@gmail.com>
2025-03-25 19:01:12 +0800

    gatau ah males

---
+++ Posttest 2 tools/src/main/java/com/kapal/model/Nahkoda.java
@@ -0,0 +1,46 @@
+package com.kapal.model;
+
+import java.util.ArrayList;
+
+public class Nahkoda {
+    protected String idNahkoda; // Bisa diakses subclass
+    String nama; // Default access modifier (hanya bisa diakses dalam package yang sama)
+    private String noTelp;
+
+    public Nahkoda(String idNahkoda, String nama, String noTelp) {
+        this.idNahkoda = idNahkoda;
+        this.nama = nama;
+        this.noTelp = noTelp;
+    }
+
+    public static void lihatNahkoda(ArrayList<Nahkoda> daftarNahkoda) {
+        System.out.println("\nDaftar Nahkoda:");
+        for (Nahkoda n : daftarNahkoda) {
+            System.out.println("ID: " + n.idNahkoda + ", Nama: " + n.nama + ", No. Telepon: " + n.noTelp);
+        }
+    }
+    
+    public String getIdNahkoda() {
+        return idNahkoda;
+    }
+
+    public void setIdNahkoda(String idNahkoda) {
+        this.idNahkoda = idNahkoda;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+    }
+}
---
+++ Posttest 2 tools/src/main/java/com/kapal/model/Penyewa.java
@@ -0,0 +1,56 @@
+package com.kapal.model;
+
+import java.util.ArrayList;
+
+public class Penyewa {
+    private String idPenyewa;
+    private String nama;
+    private String noTelp;
+    private String idKapal;
+
+    public Penyewa(String idPenyewa, String nama, String noTelp) {
+        this.idPenyewa = idPenyewa;
+        this.nama = nama;
+        this.noTelp = noTelp;
+        this.idKapal = null;
+    }
+
+    public static void lihatPenyewa(ArrayList<Penyewa> daftarPenyewa) {
+        System.out.println("\nDaftar Penyewa:");
+        for (Penyewa p : daftarPenyewa) {
+            System.out.println("ID: " + p.idPenyewa + ", Nama: " + p.nama + ", No. Telepon: " + p.noTelp + ", Kapal Disewa: " + (p.idKapal == null ? "Tidak ada" : p.idKapal));
+        }
+    }
+
+    public String getIdPenyewa() {
+        return idPenyewa;
+    }
+
+    public void setIdPenyewa(String idPenyewa) {
+        this.idPenyewa = idPenyewa;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+    }
+
+    public String getIdKapal() {
+        return idKapal;
+    }
+
+    public void setIdKapal(String idKapal) {
+        this.idKapal = idKapal;
+    }
+}
